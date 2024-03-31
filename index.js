4711789f930de58e4fce5258654f5000449ff664
Federico Peirano <fedepr2345@gmail.com>
2024-03-30 23:28:57 -0300

    a. Servidor GraphQL - Ejercicios8.2

--- index.js
+++ index.js
@@ -80,10 +80,19 @@ let books = [
 ]
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
     dummy: Int
     bookCount: Int
     authorCount: Int
+    allBooks: [Book!]!
   }
 `
 
@@ -91,7 +100,8 @@ const resolvers = {
   Query: {
     dummy: () => 0,
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 

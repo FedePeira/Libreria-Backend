80116fa8a9fcb334fcbaa095c19bf57609a0e1c1
Federico Peirano <fedepr2345@gmail.com>
2024-03-30 23:06:49 -0300

    a. Servidor GraphQL - Ejercicios8.1

--- index.js
+++ index.js
@@ -82,14 +82,19 @@ let books = [
 const typeDefs = `
   type Query {
     dummy: Int
+    bookCount: Int
+    authorCount: Int
   }
 `
 
 const resolvers = {
   Query: {
-    dummy: () => 0
+    dummy: () => 0,
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   }
 }
+
 
 const server = new ApolloServer({
   typeDefs,
